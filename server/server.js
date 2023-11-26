1665fd8475235288fbb78a27c684ba48d40df194
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-11-25 16:40:48 -0800

    Add Database schema

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
+
+
+//example code
+var conString = "INSERT_YOUR_POSTGRES_URL_HERE" //Can be found in the Details page
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
