873aa817b985411c46f828bc3c25b1ee7ec15c96
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-11-28 13:03:48 -0800

    Add Header code

---
+++ src/components/_partials/_Header.js
@@ -0,0 +1,17 @@
+import react from "react";
+
+import styles from "../styles/Header.module.css";
+import logo from "../images/CCG_logo.png";
+
+export default function Header() {
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.logoContainer}>
+        <img src={logo} className="img-logo" alt="logo" />
+      </div>
+      <div className={styles.buttonContainer}>
+        <button className="header-button">Visit our main website</button>
+      </div>
+    </div>
+  );
+}
