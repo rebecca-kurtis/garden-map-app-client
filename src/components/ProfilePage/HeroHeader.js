ed7b71f5dfb70e50a2ec847c3dd16a999d09ea06
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-12-06 15:52:31 -0800

    Add hero header code

--- src/components/ProfilePage/HeroHeader.js
+++ src/components/ProfilePage/HeroHeader.js
@@ -1,9 +1,15 @@
-import react from "react";
+import styles from "../styles/ProfilePage/HeroHeader.module.css";
+import userImage from "../images/user-image.png";
 
 export default function HeroHeader() {
   return (
-    <div>
-      Hero
+    <div className={styles.heroHeaderContainer}>
+      <div className={styles.heroHeaderImageContainer}>
+        <img src={userImage} alt="profile"></img>
+      </div>
+      <div className={styles.heroHeaderNameContainer}>
+        <h1>Meet Anne Smith</h1>
+      </div>
     </div>
   );
 }
