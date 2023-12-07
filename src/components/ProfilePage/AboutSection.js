9b7108bf6ca4540c693cee72f05f2144994dd209
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-12-06 16:08:23 -0800

    Add about section code

--- src/components/ProfilePage/AboutSection.js
+++ src/components/ProfilePage/AboutSection.js
@@ -1,9 +1,9 @@
-import react from "react";
-
+import styles from "../styles/ProfilePage/AboutSection.module.css"
 export default function AboutSection() {
   return (
-    <div>
-      Home Page
+    <div className={styles.aboutSectionContainer}>
+      <h2>About the Plot:</h2>
+      <p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.</p>
     </div>
   );
 }
--- src/components/ProfilePage/ProfilePageIndex.js
+++ src/components/ProfilePage/ProfilePageIndex.js
@@ -1,11 +1,17 @@
-import react from "react";
-import HeroHeader from "./HeroHeader";
+import styles from "../styles/ProfilePage/ProfilePageIndex.module.css"
 
+import HeroHeader from "./HeroHeader";
+import PlantsGrowing from "./PlantsGrowing";
+import AboutSection from "./AboutSection";
 
 export default function ProfilePageIndex() {
   return (
-    <div className="profilePageContainer" style={{height: "90vh"}}>
+    <div className={styles.profilePageContainer} style={{height: "90vh"}}>
       <HeroHeader />
+      <div className={styles.aboutSectionIndexContainer}>
+        <PlantsGrowing />
+        <AboutSection />
+      </div>
     </div>
   );
 }
