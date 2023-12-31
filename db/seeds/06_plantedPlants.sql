11ddfcd1db10b13669dd0079852ebdfbb3ff679a
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-12-30 21:43:12 -0800

    Add database updates

---
+++ db/seeds/06_plantedPlants.sql
@@ -0,0 +1,100 @@
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (1, 2);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (1, 6);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (1, 7);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (1, 8);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (2, 5);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (2, 11);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (2, 9);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (2, 3);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (3, 15);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (3, 33);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (3, 4);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (3, 3);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (4, 23);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (4, 10);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (4, 8);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (4, 26);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (5, 16);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (5, 25);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (5, 36);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (5, 8);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (6, 7);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (6, 1);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (6, 39);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (6, 40);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (7, 15);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (7, 21);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (7, 18);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (7, 37);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (8, 6);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (8, 19);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (8, 31);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (8, 39);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (9, 13);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (9, 5);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (9, 28);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (9, 36);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (10, 3);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (10, 14);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (10, 27);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (10, 32);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (11, 33);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (11, 4);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (11, 7);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (11, 22);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (12, 33);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (12, 16);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (12, 24);
+INSERT INTO plantedPlants (plot_id, plant_id)
+VALUES (12, 35);
+
+
+
+
