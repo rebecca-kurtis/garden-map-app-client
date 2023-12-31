2717f1c8b393b18d638a280d99f9f1708db686ce
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-12-30 21:25:54 -0800

    Update database

---
+++ db/seeds/05_plants.sql
@@ -0,0 +1,119 @@
+INSERT INTO plants (name, photo_url)
+VALUES ('apple', 'src/components/images/Plantplants/apple.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('artichoke', 'src/components/images/Plantplants/artichoke.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('asparagus', 'src/components/images/Plantplants/artichoke.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('basil', 'src/components/images/Plantplants/basil.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('beet', 'src/components/images/Plantplants/beet.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('bok-choy', 'src/components/images/Plantplants/bok-choy.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('broccoli', 'src/components/images/Plantplants/broccoli.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('cabbage', 'src/components/images/Plantplants/cabbage.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('carrot', 'src/components/images/Plantplants/carrot.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('cauliflower', 'src/components/images/Plantplants/cauliflower.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('celery', 'src/components/images/Plantplants/celery.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('chard', 'src/components/images/Plantplants/chard.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('chili-pepper', 'src/components/images/Plantplants/chili-pepper.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('chinese-cabbage', 'src/components/images/Plantplants/chinese-cabbage.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('corn', 'src/components/images/Plantplants/corn.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('cucumber', 'src/components/images/Plantplants/cucumber.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('dill', 'src/components/images/Plantplants/dill.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('eggplant', 'src/components/images/Plantplants/eggplant.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('garlic', 'src/components/images/Plantplants/garlic.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('ginger', 'src/components/images/Plantplants/ginger.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('green-beans', 'src/components/images/Plantplants/green-beans.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('green-onion', 'src/components/images/Plantplants/green-onion.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('green-pepper', 'src/components/images/Plantplants/green-pepper.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('kale', 'src/components/images/Plantplants/kale.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('lettuce', 'src/components/images/Plantplants/lettuce.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('mushroom', 'src/components/images/Plantplants/mushroom.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('mustard-greens', 'src/components/images/Plantplants/mustard-greens.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('pea', 'src/components/images/Plantplants/pea.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('potato', 'src/components/images/Plantplants/potato.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('pumpkin', 'src/components/images/Plantplants/pumpkin.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('radish', 'src/components/images/Plantplants/radish.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('raspberries', 'src/components/images/Plantplants/raspberries.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('red-onion', 'src/components/images/Plantplants/red-onion.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('spinach', 'src/components/images/Plantplants/spinach.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('strawberry', 'src/components/images/Plantplants/strawberry.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('sweet-potato', 'src/components/images/Plantplants/sweet-potato.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('tomato', 'src/components/images/Plantplants/tomato.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('yellow-onion', 'src/components/images/Plantplants/yellow-onion.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('yellow-pepper', 'src/components/images/Plantplants/yellow-pepper.png');
+
+INSERT INTO plants (name, photo_url)
+VALUES ('zucchini', 'src/components/images/Plantplants/zucchini.png');
--- db/seeds/06_plants.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO plants (name, plot_id, icon_id)
-VALUES ('Plant 1', 1, 1);
-
