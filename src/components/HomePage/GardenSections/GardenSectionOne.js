8491685df4886bac85f3f347af4050da18987f43
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2024-01-01 20:48:10 -0800

    Add plot 2 code

--- src/components/HomePage/GardenSections/GardenSectionOne.js
+++ src/components/HomePage/GardenSections/GardenSectionOne.js
@@ -1,45 +1,54 @@
-import styles from '../../styles/HomePage/GardenSectionOne.module.css';
-import CircleBlocker from '../Plots/CircleBlocker';
-import LargePlotElement from '../Plots/LargePlotElement';
-import MediumPlotElementHorizontal from '../Plots/MediumPlotElementHorizontal';
-import SmallPlotElement from '../Plots/SmallPlotElement';
+import styles from "../../styles/HomePage/GardenSectionOne.module.css";
+import CircleBlocker from "../Plots/CircleBlocker";
+import LargePlotElement from "../Plots/LargePlotElement";
+import MediumPlotElementHorizontal from "../Plots/MediumPlotElementHorizontal";
+import SmallPlotElement from "../Plots/SmallPlotElement";
 
 export default function GardenSectionOne(props) {
-
   const plantedPlantsArraySectionOne = props.plants;
   // console.log('test', plantedPlantsArraySectionOne);
   // console.log('test2', props.plantInfo)
   const plotOne = [];
   const plotOnePlants = [];
+  const plotTwo = [];
+  const plotTwoPlants = [];
 
   for (let plantObject in plantedPlantsArraySectionOne) {
     if (plantedPlantsArraySectionOne[plantObject].plot_id === 1) {
-      // console.log('num3', plantedPlantsArray[plantObject].plot_id)
       plotOne.push(plantedPlantsArraySectionOne[plantObject]);
-      
+    }
+    if (plantedPlantsArraySectionOne[plantObject].plot_id === 2) {
+      plotTwo.push(plantedPlantsArraySectionOne[plantObject]);
     }
     for (let plantInfoObject in props.plantInfo) {
-      // console.log('plant info', props.plantInfo[plantInfoObject]);
-      let conditionOne = plantedPlantsArraySectionOne[plantObject].plot_id === 1;
-      if (conditionOne && plantedPlantsArraySectionOne[plantObject].plant_id === props.plantInfo[plantInfoObject].plant_id) {
-        // console.log('num3', plantedPlantsArray[plantObject].plot_id)
+      let conditionOne =
+        plantedPlantsArraySectionOne[plantObject].plot_id === 1;
+      let conditionTwo =
+        plantedPlantsArraySectionOne[plantObject].plot_id === 2;
+      if (
+        conditionOne &&
+        plantedPlantsArraySectionOne[plantObject].plant_id ===
+          props.plantInfo[plantInfoObject].plant_id
+      ) {
         plotOnePlants.push(props.plantInfo[plantInfoObject].photo_url);
-        
       }
-      
+      if (
+        conditionTwo &&
+        plantedPlantsArraySectionOne[plantObject].plant_id ===
+          props.plantInfo[plantInfoObject].plant_id
+      ) {
+        plotTwoPlants.push(props.plantInfo[plantInfoObject].photo_url);
+      }
     }
     // console.log(plotOne);
     // console.log(plotOnePlants);
-    
-}
-
-
+  }
 
   return (
     <div className={styles.gardenSectionOneContainer}>
       <div className={styles.sec1row1}>
-        <MediumPlotElementHorizontal plants={plotOne} icons={plotOnePlants}/>
-        {/* <MediumPlotElementHorizontal /> */}
+        <MediumPlotElementHorizontal plants={plotOne} icons={plotOnePlants} />
+        <MediumPlotElementHorizontal plants={plotTwo} icons={plotTwoPlants}/>
         <CircleBlocker />
       </div>
       <div className={styles.sec1row2}>
@@ -75,8 +84,8 @@ export default function GardenSectionOne(props) {
         </div>
         <div className={styles.sec1row6col2}>
           <div className={styles.sec1row6col2row}>
-              <SmallPlotElement />
-              <SmallPlotElement />
+            <SmallPlotElement />
+            <SmallPlotElement />
           </div>
           <SmallPlotElement />
         </div>
